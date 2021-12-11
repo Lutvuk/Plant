a0afce818c08288d4c5f060bc4f0e548dc2353f2
Lutvuk <lutfir.hartanto@gmail.com>
2021-12-11 17:08:32 +0700

    Initial commit

---
+++ Plant.java
@@ -0,0 +1,85 @@
+class Plant{
+    int statusTumbuh;//0-4
+    int jumlahAir;
+    int jumlahPupuk;
+public Plant()
+    {
+        statusTumbuh = 0;
+        jumlahAir = 0;
+        jumlahPupuk = 0;
+    }
+public void beriAir()
+    {
+        jumlahAir++;
+        cekKondisiTumbuh();
+    }
+public void beriPupuk()
+    {
+        jumlahPupuk++;
+        cekKondisiTumbuh();
+    }
+public void cekKondisiTumbuh()
+    {
+        //cek kecukupan air dan pupuk
+        if(jumlahAir >=3 && jumlahPupuk >=1)
+        {
+         tumbuh();
+        }
+    }
+public void tumbuh()
+    {
+        if(statusTumbuh <4)
+        {
+            jumlahAir = jumlahAir - 3;
+            jumlahPupuk = jumlahPupuk - 1;
+            statusTumbuh++;
+        }
+    }
+public void displayPlant()
+    {
+     System.out.println(getStatusTumbuhText());
+        System.out.println("Jumlah Air:" + jumlahAir);
+        System.out.println("Jumlah Pupuk:" + jumlahPupuk);
+    }
+public String getStatusTumbuhText()
+    {
+     switch(statusTumbuh) {
+        case 0:
+        return "Benih";
+        case 1:
+        return "Tunas";
+        case 2:
+        return "Tanaman Kecil";
+        case 3:
+        return "Tanaman Dewasa";
+        }
+     return "Berbunga";
+    }
+public int getStatusTumbuh()
+    {
+        return statusTumbuh;
+    }
+public String getImagePath()
+    {
+    String tImagePath = "img/seed.png";
+        switch(statusTumbuh) {
+        case 0:
+        tImagePath = "img/seed.png";
+        break;
+        case 1:
+        tImagePath = "img/sprout.png";
+        break;
+        case 2:
+        tImagePath = "img/small.png";
+        break;
+        case 3:
+        tImagePath = "img/big.png";
+        break;
+        case 4:
+        tImagePath = "img/blossom.png";
+        break;
+        }
+        return tImagePath;
+    }
+}
+
---
+++ PlantMainSwing.java
@@ -0,0 +1,90 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.lang.Thread;
+public class PlantMainSwing{
+private JFrame frame;
+private JLabel label;
+private JButton btnBeriAir;
+private JButton btnBeriPupuk;
+private JTextField txtDisplay;
+private ImageIcon plantIcon;
+private Plant p;
+public PlantMainSwing()
+{
+p = new Plant();
+createAndShowGUI();
+setPlantImage();
+}
+private void createAndShowGUI()
+{
+frame = new JFrame("WELCOME TO UGARDEN");
+frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+frame.setLayout(new FlowLayout());
+//Add label
+label = new JLabel("");
+frame.getContentPane().add(label);
+//Add button
+btnBeriAir = new JButton("Beri Air");
+btnBeriPupuk = new JButton("Beri Pupuk");
+frame.getContentPane().add(btnBeriAir);
+frame.getContentPane().add(btnBeriPupuk);
+//Add text
+txtDisplay = new JTextField("hasilnya");
+frame.getContentPane().add(txtDisplay);
+//Add action listener
+btnBeriAir.addActionListener(new ActionListener() {
+public void actionPerformed(ActionEvent e)
+{
+btnBeriAir_actionPerformed(e);
+}
+});
+btnBeriPupuk.addActionListener(new ActionListener()
+{
+public void actionPerformed(ActionEvent e) {
+btnBeriPupuk_actionPerformed(e);
+}
+});
+//Display the window
+frame.setSize(500,300);
+frame.setVisible(true);
+}
+private void btnBeriAir_actionPerformed(ActionEvent e){
+System.out.println("beri air");
+p.beriAir();
+txtDisplay.setText(p.getStatusTumbuhText());
+setPlantImage();
+}
+private void btnBeriPupuk_actionPerformed(ActionEvent e){
+System.out.println("beri pupuk");
+p.beriPupuk();
+txtDisplay.setText(p.getStatusTumbuhText());
+setPlantImage();
+}
+protected ImageIcon createImageIcon(String path, String
+description) {
+java.net.URL imgURL = getClass().getResource(path);
+if (imgURL != null) {
+return new ImageIcon(imgURL, description);
+} else {
+System.err.println("Couldn't find file: " + path);
+return null;
+}
+}
+private void setPlantImage()
+{
+plantIcon = createImageIcon(p.getImagePath(),
+p.getStatusTumbuhText());
+label.setIcon(plantIcon);
+}
+public static void main(String[] args)
+{
+javax.swing.SwingUtilities.invokeLater(new
+Runnable(){
+public void run(){
+PlantMainSwing uGardenApp = new
+PlantMainSwing();
+}
+});
+}
+}
